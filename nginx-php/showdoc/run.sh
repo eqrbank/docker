bd3c17a0c59484cb19aa0dc3d9f1fa9b76928ec5
nichozuo <nichozuo@qq.com>
2016-12-15 21:04:04 +0800

    更新nginx-php下showdoc的run.sh

--- nginx-php/showdoc/run.sh
+++ nginx-php/showdoc/run.sh
@@ -3,6 +3,6 @@
 curl -o /tmp/showdoc.zip https://codeload.github.com/star7th/showdoc/zip/v1.2.4
 unzip -o -q /tmp/showdoc.zip -d /opt/nginx-php/html/
 rm -rf /tmp/showdoc.zip
-mv showdoc-* showdoc
+mv /opt/nginx-php/html/showdoc-* /opt/nginx-php/html/showdoc
 docker cp showdoc.conf nginx-php:/etc/nginx/sites-enabled/showdoc.conf
 docker restart nginx-php
