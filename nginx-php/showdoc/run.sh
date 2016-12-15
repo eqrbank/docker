788a2f38e554bde3066ec6bb5692a1f1726b59da
nichozuo <nichozuo@qq.com>
2016-12-15 19:45:55 +0800

    增加showdoc的nginx-php应用

---
+++ nginx-php/showdoc/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -o /tmp/showdoc.zip https://codeload.github.com/star7th/showdoc/zip/v1.2.4
+unzip -o -q /tmp/showdoc.zip -d /opt/nginx-php/html/
+mv showdoc-* showdoc
+docker cp showdoc.conf nginx-php:/etc/nginx/sites-enabled/showdoc.conf
+docker restart nginx-php
