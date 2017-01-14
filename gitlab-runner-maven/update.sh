e88b710840b399cd4c7f497086e0d7d4e7f01684
nichozuo <nichozuo@qq.com>
2017-01-14 11:32:20 +0800

    update

--- gitlab-runner-maven/update.sh
+++ gitlab-runner-maven/update.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-
-sh rm.sh
 docker rmi registry.cn-hangzhou.aliyuncs.com/nichozuo/gitlab-runner-maven:latest
 docker pull registry.cn-hangzhou.aliyuncs.com/nichozuo/gitlab-runner-maven:latest
